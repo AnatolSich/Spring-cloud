6b53ea6dec7dcdc5b418c88ac5914bf96ab35887
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-05-30 15:32:28 +0300

    Get info though integration service by date

---
+++ room-reservation-service/src/main/java/org/integration/Reservation.java
@@ -0,0 +1,15 @@
+package org.integration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+public class Reservation {
+    private long id;
+    private long roomId;
+    private long guestId;
+    private Date date;
+}

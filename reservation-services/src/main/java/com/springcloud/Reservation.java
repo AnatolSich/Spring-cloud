c45d45b416833edbd173508edf52d9f574fe100e
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-05-30 11:08:41 +0300

    Created several microservices as modules

---
+++ reservation-services/src/main/java/com/springcloud/Reservation.java
@@ -0,0 +1,29 @@
+package com.springcloud;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "RESERVATION")
+public class Reservation {
+    @Id
+    @Column(name = "RESERVATION_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "ROOM_ID")
+    private long roomId;
+    @Column(name = "GUEST_ID")
+    private long guestId;
+    @Column(name = "RES_DATE")
+    private Date date;
+}
